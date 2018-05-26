61aaa9318c94a456f3db01e1397705fe9a910518
Chris Penner <cpenner@vendasta.com>
2018-05-26 16:14:12 -0600

    All traversals return List

--- src/Traverse.purs
+++ src/Traverse.purs
@@ -6,7 +6,7 @@ import Data.Argonaut (Json, _Array, _Object, fromArray)
 import Data.Array (fromFoldable)
 import Data.Either (Either(..))
 import Data.Foldable (class Foldable)
-import Data.Lens (Traversal', _Just, preview, to, traversed, (^..), (^?))
+import Data.Lens (Traversal', _Just, _Right, preview, to, traversed, (^..), (^?))
 import Data.Lens.At (at)
 import Data.Lens.Index (ix)
 import Data.List (List(..), (:))
@@ -14,18 +14,18 @@ import Data.Maybe (Maybe(..), maybe)
 import Navigate (Navigator(..))
 import Parse (parsePath)
 
-followPath :: List Navigator -> Json -> Either String Json
+followPath :: List Navigator -> Json -> Either String (List Json)
 followPath (Key key : xs) v = let err = Left ("couldn't find " <> key)
                                   result = preview (atKey key) v
                                in (err `maybe` followPath xs) result
-followPath (Traverse : xs) v = pure <<< toArray $ v ^.. traverseArray <<< to (followPath xs) <<< traversed
+followPath (Traverse : xs) v = pure $ v ^.. traverseArray <<< to (followPath xs) <<< _Right <<< traversed
 followPath (Index n : xs) v = do
   case v ^? _Array <<< ix n of
        Nothing -> Left $ "index " <> show n <> " is out of bounds"
        Just v' -> followPath xs v'
-followPath Nil v = pure v
+followPath Nil v = pure <<< pure $ v
 
-toArray :: forall f. Foldable f => f Json -> Json
+toArray :: List Json -> Json
 toArray = fromArray <<< fromFoldable
 
 atKey :: String -> Traversal' Json Json
@@ -34,7 +34,7 @@ atKey key = _Object <<< at key <<< _Just
 traverseArray :: Traversal' Json Json
 traverseArray = _Array <<< traversed
 
-crawl :: String -> Json -> Either String Json
+crawl :: String -> Json -> Either String (List Json)
 crawl path json = do
   p <- parsePath path
   followPath p json
--- test/Main.purs
+++ test/Main.purs
@@ -6,6 +6,7 @@ import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff, kind Effect)
 import Data.Argonaut (class EncodeJson, Json, encodeJson, fromArray, fromNumber, fromString, jsonParser)
 import Data.Either (Either)
+import Data.List (fromFoldable) as L
 import Data.StrMap (fromFoldable)
 import Data.Tuple (Tuple(..))
 import Test.Spec (describe, it)
@@ -43,24 +44,24 @@ exampleJson = jsonParser $ """
 }
 """
 
-testJson :: forall a. EncodeJson a => String -> a -> _
+testJson :: forall a. EncodeJson a => String -> Array a -> _
 testJson path expected =
   let actual = exampleJson >>= crawl path
-   in actual `shouldEqual` pure (encodeJson expected)
+   in actual `shouldEqual` pure (encodeJson <$> L.fromFoldable expected)
 
 main :: Eff (RunnerEffects ()) Unit
 main = run [consoleReporter] $ do
   describe "crawl" do
     describe "handles single paths" do
-      it "finds strings" $ testJson "str" "Hello"
-      it "finds numbers" $ testJson "num" 123
-      it "finds arrays" $ testJson "arr" [1, 2, 3]
-      it "finds objects" $ testJson "obj" (fromFoldable [Tuple "a" 1])
+      it "finds strings" $ testJson "str" ["Hello"]
+      it "finds numbers" $ testJson "num" [123]
+      it "finds arrays" $ testJson "arr" [[1, 2, 3]]
+      it "finds objects" $ testJson "obj" [(fromFoldable [Tuple "a" 1])]
     describe "dives into objects" do
-      it "finds properties with ." $ testJson "obj.a" 1
-      it "finds nested properties with . chains" $ testJson "nested.deep.val" 42
+      it "finds properties with ." $ testJson "obj.a" [1]
+      it "finds nested properties with . chains" $ testJson "nested.deep.val" [42]
     describe "traverses arrays" do
       it "finds each element with []" $ testJson "arr.[]" [1, 2, 3]
       it "finds each nested element with []" $ testJson "nestedArr.[].val" [1, 2, 3]
-      it "finds indexes with .[i]" $ testJson "arr.[0]" 1
+      it "finds indexes with .[i]" $ testJson "arr.[0]" [1]
 
